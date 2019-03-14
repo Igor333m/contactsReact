d7d6ec96530b94cb0689b4cb7cdf8e89454476c6
Igor Todorovic <igor333m@yahoo.com>
2019-03-13 23:08:29 -0700

    refactor: Add ContactsAPI.js and fetch the data from database

--- src/App.js
+++ src/App.js
@@ -1,30 +1,21 @@
 import React, { Component } from 'react';
 import ListContacts from './components/ListContacts';
+import * as ContactsAPI from './utils/ContactsAPI';
 
 class App extends Component {
 
   state = {
-    contacts: [
-      {
-        "id": "karen",
-        "name": "Karen Isgrigg",
-        "handle": "karen_isgrigg",
-        "avatarURL": "http://localhost:5001/karen.jpg"
-      },
-      {
-        "id": "richard",
-        "name": "Richard Kalehoff",
-        "handle": "richardkalehoff",
-        "avatarURL": "http://localhost:5001/richard.jpg"
-      },
-      {
-        "id": "tyler",
-        "name": "Tyler McGinnis",
-        "handle": "tylermcginnis",
-        "avatarURL": "http://localhost:5001/tyler.jpg"
-      }
-     ]
+    contacts: []
   }
+
+  componentDidMount() {
+    ContactsAPI.getAll().then( contacts => {
+      this.setState( () => ({
+        contacts
+      }) );
+    } );
+  }
+
   /*
   * Remove selected contact from the list
   */
