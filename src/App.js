b3ae7058e63c0e932621cc9cca9809ab1c1e6028
Igor Todorovic <igor333m@yahoo.com>
2019-03-23 16:45:05 -0700

    feat: Add contact logic

--- src/App.js
+++ src/App.js
@@ -30,6 +30,15 @@ class App extends Component {
     ContactsAPI.remove(contact);
   }
 
+  createContact = contact => {
+    ContactsAPI.create(contact)
+      .then( (contact) => {
+        this.setState( (prevState) => ({
+          contacts: prevState.contacts.concat([contact])
+        }));
+      });
+  }
+
   render() {
     
     return (
@@ -40,7 +49,14 @@ class App extends Component {
             onDeleteContact = {this.removeContact}
           />
         )} />
-        <Route path='/create' component={ CreateContact }/>
+        <Route path='/create' render={ ({ history }) => (
+          <CreateContact
+            onCreateContact={ contact => {
+              this.createContact(contact)
+              history.push('/')
+            }}
+          />
+        )} />
       </div>
     );
   }
